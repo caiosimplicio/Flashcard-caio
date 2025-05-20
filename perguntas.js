e16651969c5af28155902dbb9d9f9c9e8450d1de
caiosimplicio <caio.simplicio.mendes@escola.pr.gov.br>
2025-05-20 14:37:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador do Brasil?',
+    'Dom Pedro 1'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Proclamação da República no Brasil?',
+    '1889'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Francesa?',
+    'Uma revolta popular que acabou com a monarquia na França em 1789'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvares Cabral'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal conflito da Segunda Guerra Mundial?',
+    'A luta etre os Aliados e o Eixo de 1939 a 1945'
+)
+
+criaCartao(
+    'História',
+    'O que foi a escravidão?',
+    'Sistema em que pessoas eram forçadas a trabalhar sem liberdade nem salário'
+)
+
